9a54eab968e7f0cba20033b1506bbddec074a113
David English <dwenglish97@outlook.com>
2025-10-29 16:10:51 -0400

    data files are now csv and began the EDA process of cleaning and exploring this dataset. Also began the process of creating a database from these files.

---
+++ database.py
@@ -0,0 +1,18 @@
+import sqlite3
+import pandas as pd
+
+# create the database
+connection = sqlite3.connect("database.db")
+
+# import files
+housing_value = pd.read_csv('data/raw/hpi_po_metro.csv')
+
+unemployment = pd.read_csv('data/raw/ssamatab1.csv', skiprows=2)
+
+# clean unemployment
+unemployment = unemployment.dropna(axis=1)
+
+# create the tables in the database
+
+unemployment.to_sql("unemployment", connection, if_exists="replace", index=False)
+housing_value.to_sql("housing_value", connection, if_exists="replace", index=False)
